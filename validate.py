8bdf314ea97a4b54d85746fbcf54a82c8e1f8e09
Robert Bjornson <robert.bjornson@yale.edu>
2015-12-29 16:50:01 -0500

    Added a small comment as part of git testing

--- validate.py
+++ validate.py
@@ -15,6 +15,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Ths program takes an Illumina samplesheet (old style, v1.8) and a config file (aka gerald.txt), and compares them to
+make sure everything matches up.
 '''
 
 
