503545d89d3747fb2694cd5decaa2936bca89f80
Robert Bjornson <robert.bjornson@yale.edu>
2017-04-06 11:52:41 -0400

    new script for NovaSeq

--- fastq.py
+++ fastq.py
@@ -1,8 +1,9 @@
+
 import sys, gzip
 
 def fastqReader(fn):
     if fn=='-':
-        fp=sys.stdin
+	fp=sys.stdin
     else:
         try:
             # test to see if it's compressed
@@ -22,35 +23,3 @@ def fastqReader(fn):
         qual=fp.readline().rstrip()
         yield (hdr1, seq, hdr2, qual)
 
-class fastqWriter:
-    def __init__(self, fn, linelen=60):
-        self.ofile=open(fn, 'w')
-        self.linelen=linelen
-    def close(self):
-        self.ofile.close()
-    def writeFQ(self, hdr1, seq, hdr2, qual):
-        pos=0
-        assert(len(seq)==len(qual))
-        stop=len(seq)
-        self.ofile.write(hdr1)
-        self.ofile.write('\n')
-        while pos<stop:
-            self.ofile.write(seq[pos:pos+self.linelen])
-            self.ofile.write('\n')
-            pos+=self.linelen
-
-        pos=0
-        stop=len(qual)
-        self.ofile.write(hdr2)
-        self.ofile.write('\n')
-        while pos<stop:
-            self.ofile.write(qual[pos:pos+self.linelen])
-            self.ofile.write('\n')
-            pos+=self.linelen
-
-if __name__=='__main__':
-    rdr=fastqReader(sys.argv[1])
-    wrter=fastqWriter(sys.argv[2], 9999)
-    for hdr1, seq, hdr2, qual in rdr:
-        wrter.writeFQ(hdr1, seq, hdr2, qual)
-    wrter.close()
