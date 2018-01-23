bb272bcf869b6c8d4e6dfe5bbfc66a51dd65f4c6
Robert Bjornson <rbjornson@gmail.com>
2018-01-23 14:01:44 -0500

    new

--- sendQuotas.sh
+++ sendQuotas.sh
@@ -2,3 +2,5 @@
 
 grep pi_gerstein /ysm-gpfs/.mmrepquota/current > /tmp/gerstein.txt && echo "gerstein PI quota report" | mail -a /tmp/gerstein.txt -c robert.bjornson@yale.edu -s "gerstein PI quota report" gamze.gursoy@yale.edu && rm /tmp/gerstein.txt
 
+grep pi_zhao /ysm-gpfs/.mmrepquota/current > /tmp/zhao.txt && echo "zhao PI quota report" | mail -a /tmp/zhao.txt -c robert.bjornson@yale.edu -s "Zhao quota report" dingjue.ji@yale.edu && rm /tmp/zhao.txt
+
